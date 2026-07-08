fe804ae2f94df24dca534122df1eedc190bd4c13
Dennes Njoroge <dennesmwangi55@gmail.com>
2026-07-08 23:18:44 +0300

    feat: implement properties filtering at the db layer

--- primelet-core/src/modules/properties/prop.repository.js
+++ primelet-core/src/modules/properties/prop.repository.js
@@ -2,8 +2,42 @@ import pool from "../../config/db.js";
 import AppError from "../../utils/AppError.js";
 import utils from "../../utils/utils.js";
 
-const fetchAll = async () => {
-  const [rows] = await pool.execute(`SELECT * FROM houses`);
+const fetchAll = async (filters) => {
+  console.log(filters);
+  let sql = "SELECT * FROM houses WHERE 1=1";
+  const params = [];
+
+  if (filters.minPrice !== undefined) {
+    sql += " AND price >= ?";
+    params.push(filters.minPrice);
+  }
+
+  if (filters.maxPrice !== undefined) {
+    sql += " AND price <= ?";
+    params.push(filters.maxPrice);
+  }
+
+  if (filters.beds !== undefined) {
+    sql += " AND beds = ?";
+    params.push(filters.beds);
+  }
+
+  if (filters.bath !== undefined) {
+    sql += " AND bath = ?";
+    params.push(filters.bath);
+  }
+
+  if (filters.parking !== undefined) {
+    sql += " AND parking = ?";
+    params.push(filters.parking);
+  }
+
+  if (filters.county) {
+    sql += " AND county = ?";
+    params.push(filters.county);
+  }
+
+  const [rows] = await pool.execute(sql, params);
 
   if (rows.length === 0) {
     throw new AppError("No properties found", 404);
